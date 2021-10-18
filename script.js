d7d2ea23150247fe0440a1ccb0bb3e235d7e0739
shaik-md-ibnay-momen <shaikmohammad864@gmail.com>
2021-10-18 16:18:45 +0600

    calculation fixing

--- script.js
+++ script.js
@@ -16,38 +16,40 @@ function year_slice(mainDate){
     year= mainDate.slice(0,4)
     year= parseInt(year);
     //console.log (typeof(year));
-    console.log(year);
 }
 
 function month_slice(mainDate){
 
     month= mainDate.slice(5,7)
     month= parseInt(month);
-    console.log(month);
 }
 
 function day_slice(mainDate){
 
     day= mainDate.slice(8,10)
     day= parseInt(day);
-    console.log(day);
 
     today_date();
 }
 
 function today_date(){
-
     var today = new Date();
     dd = String(today.getDate()).padStart(2, '0');
     mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     yyyy = today.getFullYear();
+    dd= parseInt(dd);
     calculation();
 }
 
 function calculation(){
     mYear= (yyyy-year)-1;
-    mMonth = (mm-1)+(month-1);
-    mDay = dd+day;
-    var totalDay= (mYear*365)+ (mMonth*30)+ mDay;
-    console.log(today_date)
+    mMonth = (mm-1)+(12-month);
+    mDay = dd+(30-day);
+    console.log(mYear*365 + 2);
+    console.log(typeof mYear);
+    console.log(typeof mMonth);
+    console.log(typeof day);
+    var totalDay= ((mYear*365)+ (mMonth*30)+ mDay);
+    console.log(typeof totalDay);
+    console.log(totalDay);
 }
